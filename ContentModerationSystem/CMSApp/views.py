6981e1ce4676a4e4b262f90af154b50033f2a6a7
Shaunak Vyas <shuanakvyas56@gmail.com>
2020-10-28 16:41:25 +0530

    templates and urls

--- ContentModerationSystem/CMSApp/views.py
+++ ContentModerationSystem/CMSApp/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import *
+
+
+def ProfilePage(request):
+
+    try:
+        context = {}
+        return render(request, 'CMSApp/profile.html', context)
+    except Exception as e:
+        print(str(e))
+        return HttpResponse('Internal Error')
